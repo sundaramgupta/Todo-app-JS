dd6992e9430e7559a945cca58dec6716f123eacc
Sundaram Gupta <sundaramgupta18@gmail.com>
2020-05-24 17:25:05 +0530

    initial commit

---
+++ todo.js
@@ -0,0 +1,22 @@
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  document.querySelector('#new-task').onsubmit = () => {
+
+      // Create new item for list
+      const li = document.createElement('li');
+      li.innerHTML = document.querySelector('#task').value;
+
+      // Add new item to task list
+      document.querySelector('#tasks').append(li);
+
+      // Clear input field
+      document.querySelector('#task').value = '';
+
+      // Stop form from submitting
+      return false;
+  };
+
+});
